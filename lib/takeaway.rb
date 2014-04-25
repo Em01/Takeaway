50d5a4f448ab2540e960be8122df620e60b3cb8c
Emma <emma_williams1@hotmail.co.uk>
2014-04-25 20:52:36 +0100

    first stages of spec and lib takeaway files with all tests passing

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,10 +1,26 @@
 class Takeaway 
 
-def menu 
-{ "Lasagne" => 5, "Bolognase" => 4, "Carbonara" => 6, "Seafood" => 8 }
+def cuisine
+ { :lasagne => 8, :bolognase => 6, :carbonara => 5, :seafood => 4 }
+end
+
+def placing_order(cuisine_and_quantities, order_total)
+the_price = review_the_price_of(cuisine_and_quantities)
+raise_order_total_error if the_price != order_total
+true
 end
 
 
+def review_the_price_of(cuisine_and_quantities)
+  cuisine_and_quantities.inject(0) do |price, (dish, quantity)|
+    price += cuisine[dish] * quantity
+  end
+end
+
+def raise_order_total_error
+  raise RuntimeError, 'Sorry you have input the wrong total amount. Please try and order again.'
+  end
+
 
 
 
@@ -17,5 +33,6 @@ end
 
 
 end
+
 
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,12 +4,28 @@ describe Takeaway do
 
 let(:takeaway) { Takeaway.new }
 
-context 'The restaurant should have a menu' do 
 
-          it ' the menu' do 
-            menu = { "Lasagne" => 5, "Bolognase" => 4, "Carbonara" => 6, "Seafood" => 8 }
-            expect(takeaway.menu).to eq menu
+
+          it 'should be a menu' do 
+            cuisine = { :lasagne => 8, :bolognase => 6, :carbonara => 5, :seafood => 4 }
+            expect(takeaway.cuisine).to eq cuisine
           end
+
+
+         it 'placing an order' do 
+            expect(takeaway.placing_order({ :lasagne => 1, :seafood => 2 }, 16)).to be_true
+          end
+
+          it 'if an order is placed with the wrong total value then an error should arise' do
+            expect {
+              takeaway.placing_order({ :seafood => 4 }, 1)
+              }.to raise_error(RuntimeError, 'Sorry you have input the wrong total amount. Please try and order again.')
+            
         end
 
-end
+      
+
+
+
+
+end 
